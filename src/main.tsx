13f8137cfb40f1f71f98245eea9dc49345562ead
zagzy8776 <127265311+zagzy8776@users.noreply.github.com>
2026-09-20 15:13:02 +0200

    Restore auth providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><App/></BrowserRouter></React.StrictMode>);
+import React from 'react';import ReactDOM from 'react-dom/client';import {BrowserRouter} from 'react-router-dom';import App from './App';import {AuthProvider} from './context/AuthContext';import {AdminAuthProvider} from './context/AdminAuthContext';import './index.css';ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><AuthProvider><AdminAuthProvider><App/></AdminAuthProvider></AuthProvider></BrowserRouter></React.StrictMode>);
